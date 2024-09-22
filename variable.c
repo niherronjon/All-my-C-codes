f4c46a17b561009ab4322ee69b53b6217de658e6
Niher Ronjon Pramanik <130027941+niherronjon@users.noreply.github.com>
2024-09-22 17:51:06 +0600

    Add files via upload

---
+++ variable.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    /*int rahim=100;
+    int karim=200;
+    float chol=2.33;
+    printf ("%d %d %0.4f",rahim,karim,chol);*/
+    char rahi='a';
+    printf("%c\n",rahi);
+    return 0;
+}
