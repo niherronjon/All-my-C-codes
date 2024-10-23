9dcf6e542d33ceee4a317dd053c304ae2c89df16
Niher Ronjon Pramanik <130027941+niherronjon@users.noreply.github.com>
2024-10-23 19:20:18 +0600

    Add files via upload

---
+++ C Basic Codes/scanf1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int num1;
+    int num2;
+    printf("Enter two numbers: ");
+    printf("Enter the first integer : ");
+    scanf("%d",&num1);
+    printf("\n");
+    printf("Enter second number :");
+    scanf("%d",&num2);
+    return 0;
+}
