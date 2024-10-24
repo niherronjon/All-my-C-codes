8285968b52aea984bc085846e4a39fb21dd592a9
Niher Ronjon Pramanik <130027941+niherronjon@users.noreply.github.com>
2024-10-24 22:40:24 +0600

    Add files via upload

---
+++ C Basic Codes/Variables/variablefloat1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    float num1=10.7;
+    double num2=10.37653;
+    char ch='a';
+    printf("num1=%f\n",num1);
+    printf("num2=%lf\n",num2);
+    printf("ch=%c,ch");
+    return 0;
+}
